9827153e2eda84e7c8b0cafc0857d29781a9a8e9
Ephraimdevelops <ephraimdevelops@gmail.com>
2022-09-02 03:24:20 +0300

    eslint changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: ["localhost"],
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
